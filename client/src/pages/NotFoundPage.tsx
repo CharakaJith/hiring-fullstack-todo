420ec2d600a1120b49d60bcfae4cbf1ba5376e90
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-11-08 17:56:33 +0530

    client: not found page implemented

---
+++ client/src/pages/NotFoundPage.tsx
@@ -0,0 +1,31 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../components/ui/button';
+
+const NotFoundPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate('/');
+  };
+
+  return (
+    <div className="fixed inset-0 w-full h-full flex flex-col justify-end items-center">
+      {/* background image */}
+      <div className="absolute inset-0 bg-cover bg-center z-0" style={{ backgroundImage: `url(/src/assets/images/notfound.webp)` }} />
+
+      {/* top text */}
+      <div className="relative z-10 mt-10 text-center">
+        <h1 className="text-5xl font-bold text-white">Page Not Found</h1>
+      </div>
+
+      {/* button at bottom */}
+      <div className="relative z-10 mb-15">
+        <Button className="bg-blue-800 hover:bg-blue-950 cursor-pointer text-xl py-5 px-6" onClick={handleGoBack}>
+          Go back
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,10 +1,15 @@
-import HomePage from '@/pages/HomePage';
 import { Routes, Route } from 'react-router-dom';
+
+import HomePage from '@/pages/HomePage';
+import NotFoundPage from '@/pages/NotFoundPage';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+
+      {/* default */}
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
